1ad5fb4b2f12ee8ce173341986d3c316e4c28dc0
ShaikTabassum1431 <140260196+ShaikTabassum1431@users.noreply.github.com>
2025-07-06 23:17:50 +0530

    3rd commit

--- server/settings.py
+++ server/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from mongoengine import connect # type: ignore
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -73,13 +74,20 @@ WSGI_APPLICATION = 'server.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+connect(
+    db='worktrack',
+    host='mongodb+srv://anyjobhub:salmankhan@cluster0.jbwhbeu.mongodb.net/worktrack',
+    username='anyjobhub',
+    password='salmankhan',
+    authentication_source='admin',
+    authentication_mechanism='SCRAM-SHA-1'
+)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
